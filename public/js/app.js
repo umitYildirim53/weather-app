ea520fbe8e65530507705cd9439d633095a873c3
Ümit Yıldırım <umit.yildirim53@hotmail.com>
2022-07-24 20:13:59 +0300

    setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 //console.log("client side js file is loaded");
 
-fetch('http://puzzle.mead.io/puzzle1').then((response) => {
+fetch('http://puzzle.mead.io/puzzle').then((response) => {
     response.json().then((data) => {
         console.log(data)
     })
